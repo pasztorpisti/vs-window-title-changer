615cdf1f985133aaead135f91c9671306f43f667
pasztorpisti@gmail.com <pasztorpisti@gmail.com@afe9cb72-5dbc-4e0f-f29d-b8c4c4a8fe90>
2013-11-11 23:57:36 +0000

    Importing the source code of VSWindowTitleChanger

---
+++ trunk/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace VSWindowTitleChanger
+{
+    static class GuidList
+    {
+        public const string guidVSWindowTitleChangerPkgString = "1e0c061a-af97-43bb-930d-7e3629676249";
+        public const string guidVSWindowTitleChangerCmdSetString = "15ae04f3-70b2-4f36-a188-48782d195223";
+
+        public static readonly Guid guidVSWindowTitleChangerCmdSet = new Guid(guidVSWindowTitleChangerCmdSetString);
+    };
+}
