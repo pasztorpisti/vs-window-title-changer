7e0131a118a1b9c99ab0cbcde65cf166297dbcbc
pasztorpisti@gmail.com <pasztorpisti@gmail.com@afe9cb72-5dbc-4e0f-f29d-b8c4c4a8fe90>
2013-12-28 01:23:56 +0000

    bugfixing some visual studio state query crashes that happend due to some exceptions thrown by VS dte interfaces. another issue that was fixed is querying the startup project correctly even if it is under a solution folder.

--- trunk/PackageGlobals.cs
+++ trunk/PackageGlobals.cs
@@ -1,15 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
+using System.Text;
 using System.Text.RegularExpressions;
+using System.Windows.Forms;
 using EnvDTE;
 using EnvDTE80;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell.Interop;
 using VSWindowTitleChanger.ExpressionEvaluator;
-using System.Windows.Forms;
-using System.Text;
 
 namespace VSWindowTitleChanger
 {
@@ -189,93 +190,301 @@ namespace VSWindowTitleChanger
 		}
 
 
-		void SetVariableValuesFromIDEState(IVariableValueSetter var_value_setter, VSMultiInstanceInfo multi_instance_info)
+
+		public static Project GetStartupProject(Solution solution)
+		{
+			Project ret = null;
+
+			if (solution != null &&
+				solution.SolutionBuild != null &&
+				solution.SolutionBuild.StartupProjects != null)
+			{
+				string uniqueName = (string)((object[])solution.SolutionBuild.StartupProjects)[0];
+
+				// Can't use the solution.Item(uniqueName) here since that doesn't work 
+				// for projects under solution folders. 
+
+				ret = GetProject(solution, uniqueName);
+			}
+
+			return ret;
+		}
+
+		public static Project GetProject(Solution solution, string uniqueName)
+		{
+			Project ret = null;
+
+			if (solution != null && uniqueName != null)
+			{
+				foreach (Project p in solution.Projects)
+				{
+					ret = GetSubProject(p, uniqueName);
+
+					if (ret != null)
+						break;
+				}
+			}
+
+			return ret;
+		}
+
+		public static Project GetSubProject(Project project, string uniqueName)
+		{
+			Project ret = null;
+
+			if (project != null)
+			{
+				if (project.UniqueName == uniqueName)
+				{
+					ret = project;
+				}
+				else if (project.Kind == EnvDTE.Constants.vsProjectKindSolutionItems)
+				{
+					// Solution folder 
+
+					foreach (ProjectItem projectItem in project.ProjectItems)
+					{
+						ret = GetSubProject(projectItem.SubProject, uniqueName);
+
+						if (ret != null)
+							break;
+					}
+				}
+			}
+
+			return ret;
+		}
+
+		class VarValues
+		{
+			public string sln_path = "";
+			public DBGMODE dbgmode = DBGMODE.DBGMODE_Design;
+			public bool sln_dirty = false;
+			public string configuration = "";
+			public string platform = "";
+			public string startup_proj = "";
+			public string startup_proj_path = "";
+			public bool startup_proj_dirty = false;
+			public string doc_path = "";
+			public bool doc_dirty = false;
+			public bool any_doc_dirty = false;
+			public bool any_proj_dirty = false;
+			public bool wnd_minimized = false;
+			public bool wnd_foreground = false;
+			public bool app_active = false;
+			public string active_wnd_title = "";
+			public string active_wnd_class = "";
+			public string orig_title = "";
+
+			public List<Exception> exceptions = new List<Exception>();
+		}
+
+		Project FindProjectObjectInSolutionFolder(Project solution_folder, object proj_id)
+		{
+			Project proj;
+			try
+			{
+				proj = solution_folder.ProjectItems.Item(proj_id).SubProject;
+			}
+			catch
+			{
+				proj = null;
+			}
+
+			if (proj != null)
+				return proj;
+
+			for (int i=1,e=solution_folder.ProjectItems.Count; i<=e; ++i)
+			{
+				Project sub_proj = solution_folder.ProjectItems.Item(i).SubProject;
+				if (sub_proj == null || sub_proj.Kind != ProjectKinds.vsProjectKindSolutionFolder)
+					continue;
+				proj = FindProjectObjectInSolutionFolder(sub_proj, proj_id);
+				if (proj != null)
+					return proj;
+			}
+
+			return null;
+		}
+
+		void GetVariableValues(VarValues var_values)
 		{
 			DTE2 dte = (DTE2)m_Package.GetInterface(typeof(DTE));
 			IVsSolution vs_solution = (IVsSolution)m_Package.GetInterface(typeof(IVsSolution));
-			string solution_path, temp_solution_dir, temp_solution_options;
-			if (VSConstants.S_OK != vs_solution.GetSolutionInfo(out temp_solution_dir, out solution_path, out temp_solution_options) || solution_path == null)
-				solution_path = "";
+			string temp_solution_dir, temp_solution_options;
+			if (VSConstants.S_OK != vs_solution.GetSolutionInfo(out temp_solution_dir, out var_values.sln_path, out temp_solution_options) || var_values.sln_path == null)
+				var_values.sln_path = "";
 
 			IVsDebugger debugger = (IVsDebugger)m_Package.GetInterface(typeof(IVsDebugger));
 			DBGMODE[] adbgmode = new DBGMODE[] { DBGMODE.DBGMODE_Design };
 			if (VSConstants.S_OK != debugger.GetMode(adbgmode))
 				adbgmode[0] = DBGMODE.DBGMODE_Design;
-			DBGMODE dbgmode = adbgmode[0] & ~DBGMODE.DBGMODE_EncMask;
+			var_values.dbgmode = adbgmode[0] & ~DBGMODE.DBGMODE_EncMask;
 
-			string active_configuration = "";
-			string active_platform = "";
-			SolutionConfiguration2 active_cfg = (SolutionConfiguration2)dte.Solution.SolutionBuild.ActiveConfiguration;
-			if (active_cfg != null)
-			{
-				active_configuration = active_cfg.Name == null ? "" : active_cfg.Name; ;
-				active_platform = active_cfg.PlatformName == null ? "" : active_cfg.PlatformName;
-			}
+			var_values.sln_dirty = !dte.Solution.Saved;
 
-			Project startup_project = null;
-			if (dte.Solution.SolutionBuild.StartupProjects != null)
-			{
-				Array projects = (Array)dte.Solution.SolutionBuild.StartupProjects;
-				if (projects.Length > 0)
-					startup_project = dte.Solution.Item(projects.GetValue(0));
-			}
-
-			Document active_document;
 			try
 			{
-				active_document = dte.ActiveDocument;
-			}
-			catch
-			{
-				active_document = null;
-			}
-
-			AddFilePathVars(var_value_setter, ref solution_path, SlashPathSeparator, "sln_");
-			var_value_setter.SetVariable("sln_open", solution_path.Length > 0);
-			bool sln_dirty = !dte.Solution.Saved;
-			var_value_setter.SetVariable("sln_dirty", sln_dirty);
-
-			string active_document_path = active_document == null ? "" : active_document.FullName;
-			AddFilePathVars(var_value_setter, ref active_document_path, SlashPathSeparator, "doc_");
-			var_value_setter.SetVariable("doc_open", active_document_path.Length > 0);
-			var_value_setter.SetVariable("doc_dirty", active_document != null && !active_document.Saved);
-
-			bool any_doc_dirty = false;
-			foreach (Document doc in dte.Documents)
-			{
-				if (!doc.Saved)
+				SolutionConfiguration2 active_cfg = (SolutionConfiguration2)dte.Solution.SolutionBuild.ActiveConfiguration;
+				if (active_cfg != null)
 				{
-					any_doc_dirty = true;
-					break;
+					var_values.configuration = active_cfg.Name == null ? "" : active_cfg.Name; ;
+					var_values.platform = active_cfg.PlatformName == null ? "" : active_cfg.PlatformName;
 				}
 			}
-			var_value_setter.SetVariable("any_doc_dirty", any_doc_dirty);
-
-			string startup_project_path = startup_project == null ? "" : startup_project.FullName;
-			AddFilePathVars(var_value_setter, ref startup_project_path, SlashPathSeparator, "startup_proj_");
-			var_value_setter.SetVariable("startup_proj", startup_project == null ? "" : startup_project.Name);
-			var_value_setter.SetVariable("startup_proj_dirty", startup_project != null && !startup_project.Saved);
-
-			bool any_proj_dirty = false;
-			foreach (Project proj in dte.Solution.Projects)
+			catch (System.Exception ex)
 			{
-				if (!proj.Saved)
+				var_values.exceptions.Add(ex);
+			}
+
+			try
+			{
+				Project startup_project = GetStartupProject(dte.Solution);
+				if (startup_project != null)
 				{
-					any_proj_dirty = true;
-					break;
+					var_values.startup_proj = startup_project.Name;
+					var_values.startup_proj_path = startup_project.FullName;
+					var_values.startup_proj_dirty = !startup_project.Saved;
 				}
 			}
-			var_value_setter.SetVariable("any_proj_dirty", any_proj_dirty);
+			catch (System.Exception ex)
+			{
+				var_values.exceptions.Add(ex);
+			}
 
-			var_value_setter.SetVariable("anything_dirty", sln_dirty || any_proj_dirty || any_doc_dirty);
+			try
+			{
+				Document active_document = dte.ActiveDocument;
+				if (active_document != null)
+				{
+					var_values.doc_path = active_document.FullName;
+					var_values.doc_dirty = !active_document.Saved;
+				}
+			}
+			catch (System.Exception ex)
+			{
+				var_values.exceptions.Add(ex);
+			}
 
-			var_value_setter.SetVariable("wnd_minimized", m_Package.VSMainWindow.Minimized);
-			var_value_setter.SetVariable("wnd_foreground", m_Package.VSMainWindow.IsForegroundWindow());
-			var_value_setter.SetVariable("app_active", m_Package.VSMainWindow.IsAppActive);
+			try
+			{
+				foreach (Document doc in dte.Documents)
+				{
+					if (!doc.Saved)
+					{
+						var_values.any_doc_dirty = true;
+						break;
+					}
+				}
+			}
+			catch (System.Exception ex)
+			{
+				var_values.exceptions.Add(ex);
+			}
+
+			try
+			{
+				foreach (Project proj in dte.Solution.Projects)
+				{
+					if (!proj.Saved)
+					{
+						var_values.any_proj_dirty = true;
+						break;
+					}
+				}
+			}
+			catch (System.Exception ex)
+			{
+				var_values.exceptions.Add(ex);
+			}
+
+			try
+			{
+				var_values.wnd_minimized = m_Package.VSMainWindow.Minimized;
+				var_values.wnd_foreground = m_Package.VSMainWindow.IsForegroundWindow();
+				var_values.app_active = m_Package.VSMainWindow.IsAppActive;
+			}
+			catch (System.Exception ex)
+			{
+				var_values.exceptions.Add(ex);
+			}
+
+			IntPtr active_wnd = GetActiveWindow();
+			if (active_wnd != IntPtr.Zero)
+			{
+				var_values.active_wnd_title = GetWindowText(active_wnd);
+				var_values.active_wnd_class = GetWindowClassName(active_wnd);
+			}
+
+			var_values.orig_title = m_Package.VSMainWindow.OriginalTitle;
+		}
+
+
+		void SetVariableValuesFromIDEState(IVariableValueSetter var_value_setter, VSMultiInstanceInfo multi_instance_info)
+		{
+			VarValues var_values = new VarValues();
+			try
+			{
+				GetVariableValues(var_values);
+			}
+			catch (System.Exception ex)
+			{
+				var_values.exceptions.Add(ex);
+			}
+
+			if (var_values.exceptions.Count > 0)
+			{
+				OutputWindow output_window = null;
+				try
+				{
+					DTE2 dte = (DTE2)m_Package.GetInterface(typeof(DTE));
+					Window window = dte.Windows.Item(EnvDTE.Constants.vsWindowKindOutput);
+					output_window = (OutputWindow)window.Object;
+				}
+				catch
+				{
+				}
+
+				foreach (Exception ex in var_values.exceptions)
+				{
+					string msg = "----------------- VSWindowTitleChanger Exception: " + ex.ToString();
+					Debug.WriteLine(msg);
+					if (output_window != null)
+					{
+						try
+						{
+							output_window.ActivePane.OutputString("\n" + msg + "\n");
+						}
+						catch
+						{
+						}
+					}
+				}
+			}
+
+			AddFilePathVars(var_value_setter, ref var_values.sln_path, SlashPathSeparator, "sln_");
+			var_value_setter.SetVariable("sln_open", var_values.sln_path.Length > 0);
+			var_value_setter.SetVariable("sln_dirty", var_values.sln_dirty);
+
+			AddFilePathVars(var_value_setter, ref var_values.doc_path, SlashPathSeparator, "doc_");
+			var_value_setter.SetVariable("doc_open", var_values.doc_path.Length > 0);
+			var_value_setter.SetVariable("doc_dirty", var_values.doc_dirty);
+			var_value_setter.SetVariable("any_doc_dirty", var_values.any_doc_dirty);
+
+			AddFilePathVars(var_value_setter, ref var_values.startup_proj_path, SlashPathSeparator, "startup_proj_");
+			var_value_setter.SetVariable("startup_proj", var_values.startup_proj);
+			var_value_setter.SetVariable("startup_proj_dirty", var_values.startup_proj_dirty);
+
+			var_value_setter.SetVariable("any_proj_dirty", var_values.any_proj_dirty);
+			var_value_setter.SetVariable("anything_dirty", var_values.sln_dirty || var_values.any_proj_dirty || var_values.any_doc_dirty);
+
+			var_value_setter.SetVariable("wnd_minimized", var_values.wnd_minimized);
+			var_value_setter.SetVariable("wnd_foreground", var_values.wnd_foreground);
+			var_value_setter.SetVariable("app_active", var_values.app_active);
 
 			bool debugging = false;
 			string debug_mode = "";
-			switch (dbgmode)
+			switch (var_values.dbgmode)
 			{
 				case DBGMODE.DBGMODE_Run:
 					debugging = true;
@@ -290,24 +499,16 @@ namespace VSWindowTitleChanger
 			var_value_setter.SetVariable("debugging", debugging);
 			var_value_setter.SetVariable("debug_mode", debug_mode);
 
-			var_value_setter.SetVariable("configuration", active_configuration);
-			var_value_setter.SetVariable("platform", active_platform);
+			var_value_setter.SetVariable("configuration", var_values.configuration);
+			var_value_setter.SetVariable("platform", var_values.platform);
 
-			var_value_setter.SetVariable("orig_title", m_Package.VSMainWindow.OriginalTitle);
+			var_value_setter.SetVariable("orig_title", var_values.orig_title);
 
 			var_value_setter.SetVariable("multi_instances", multi_instance_info.multiple_instances);
 			var_value_setter.SetVariable("multi_instances_same_ver", multi_instance_info.multiple_instances_same_version);
 
-			string active_wnd_title = "";
-			string active_wnd_class = "";
-			IntPtr active_wnd = GetActiveWindow();
-			if (active_wnd != IntPtr.Zero)
-			{
-				active_wnd_title = GetWindowText(active_wnd);
-				active_wnd_class = GetWindowClassName(active_wnd);
-			}
-			var_value_setter.SetVariable("active_wnd_title", active_wnd_title);
-			var_value_setter.SetVariable("active_wnd_class", active_wnd_class);
+			var_value_setter.SetVariable("active_wnd_title", var_values.active_wnd_title);
+			var_value_setter.SetVariable("active_wnd_class", var_values.active_wnd_class);
 		}
 
 
@@ -336,47 +537,55 @@ namespace VSWindowTitleChanger
 			vs_instance_info.multiple_instances = false;
 			vs_instance_info.multiple_instances_same_version = false;
 
-			IRunningObjectTable running_object_table;
-			if (VSConstants.S_OK != GetRunningObjectTable(0, out running_object_table))
-				return;
-			IEnumMoniker moniker_enumerator;
-			running_object_table.EnumRunning(out moniker_enumerator);
-			moniker_enumerator.Reset();
-
-			IMoniker[] monikers = new IMoniker[1];
-			IntPtr num_fetched = IntPtr.Zero;
-			int dte_count = 0;
-			int dte_count_our_version = 0;
-			while (VSConstants.S_OK == moniker_enumerator.Next(1, monikers, num_fetched))
+			try
 			{
-				IBindCtx ctx;
-				if (VSConstants.S_OK != CreateBindCtx(0, out ctx))
-					continue;
+				IRunningObjectTable running_object_table;
+				if (VSConstants.S_OK != GetRunningObjectTable(0, out running_object_table))
+					return;
+				IEnumMoniker moniker_enumerator;
+				running_object_table.EnumRunning(out moniker_enumerator);
+				moniker_enumerator.Reset();
 
-				string name;
-				monikers[0].GetDisplayName(ctx, null, out name);
-				if (!name.StartsWith("!VisualStudio.DTE."))
-					continue;
-
-				object com_object;
-				if (VSConstants.S_OK != running_object_table.GetObject(monikers[0], out com_object))
-					continue;
-
-				DTE2 dte = com_object as DTE2;
-				if (dte != null)
+				IMoniker[] monikers = new IMoniker[1];
+				IntPtr num_fetched = IntPtr.Zero;
+				int dte_count = 0;
+				int dte_count_our_version = 0;
+				while (VSConstants.S_OK == moniker_enumerator.Next(1, monikers, num_fetched))
 				{
-					++dte_count;
-					Match m = m_DTEComObjectNameRegex.Match(name);
-					if (m.Success)
+					IBindCtx ctx;
+					if (VSConstants.S_OK != CreateBindCtx(0, out ctx))
+						continue;
+
+					string name;
+					monikers[0].GetDisplayName(ctx, null, out name);
+					if (!name.StartsWith("!VisualStudio.DTE."))
+						continue;
+
+					object com_object;
+					if (VSConstants.S_OK != running_object_table.GetObject(monikers[0], out com_object))
+						continue;
+
+					DTE2 dte = com_object as DTE2;
+					if (dte != null)
 					{
-						Group g = m.Groups["dte_version"];
-						if (g.Success && g.Value == our_dte_version)
-							++dte_count_our_version;
+						++dte_count;
+						Match m = m_DTEComObjectNameRegex.Match(name);
+						if (m.Success)
+						{
+							Group g = m.Groups["dte_version"];
+							if (g.Success && g.Value == our_dte_version)
+								++dte_count_our_version;
+						}
 					}
 				}
+				vs_instance_info.multiple_instances = dte_count > 1;
+				vs_instance_info.multiple_instances_same_version = dte_count_our_version > 1;
 			}
-			vs_instance_info.multiple_instances = dte_count > 1;
-			vs_instance_info.multiple_instances_same_version = dte_count_our_version > 1;
+			catch
+			{
+				vs_instance_info.multiple_instances = false;
+				vs_instance_info.multiple_instances_same_version = false;
+			}
 		}
 
 		public EvalContext CreateFreshEvalContext()
